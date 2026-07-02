c629a0cb8aab9a3fb58eb25da4e59fbd47e10694
Justin Diaz <justhewebdev@gmail.com>
2026-07-02 08:25:58 -0400

    added transaction helper

--- server/db/db.js
+++ server/db/db.js
@@ -9,6 +9,17 @@ async function query(sql, params = []) {
     return result;
 }
 
-
+async function transaction(callback) {
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        await callback(client);
+        await client.query('COMMIT');
+    } catch (error) {
+        await client.query('ROLLBACK');
+    } finally {
+        client.release();
+    }
+}
 
 export default { query };
